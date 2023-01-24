32141ae565346207b0c80434d8dda196e0bef57f
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-01-24 17:42:12 +0100

    final commit backend_tarhely

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,44 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
+  Param,
+  Post,
+  Redirect,
   Render,
+  Session,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import db from './db';
+import TarhelyDataDto from './tarhelydata.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  @Get('/api/tarhely')
+  async allTarhely() {
+    const [storages] = await db.execute(
+      'SELECT * FROM tarhelycsomagok'
+    );
+    return { storages: storages };
+  }
+
+  @Post('/api/tarhely')
+  async registerTarhely(@Body() tarhelydata: TarhelyDataDto) {
+    await db.execute('INSERT INTO tarhelycsomagok (nev, meret, ar) VALUES (?, ?)', [
+      tarhelydata.nev,
+      tarhelydata.meret,
+      tarhelydata.ar,
+    ]);
+  }
+
+  @Delete('/api/tarhely/:id')
+  async deleteTarhely(@Param('id') id: number) {
+    await db.execute(
+      'DELETE FROM tarhelycsomagok WHERE id = ?',
+      [id],
+    );
   }
 }
---
+++ src/tarhelydata.dto.ts
@@ -0,0 +1,6 @@
+export default class TarhelyDataDto {
+    id: number;
+    nev: string;
+    meret: number;
+    ar: number;
+}
